f2fe08ef474ae4a893c5366e1ac0db91961b4dad
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-03-13 01:18:10 +0900

    add F and install Create proconio

---
+++ study-APG4b/F/src/main.rs
@@ -0,0 +1,16 @@
+fn main(){
+    proconio::input! {
+        a: i32,
+        b: i32,
+
+    }
+
+    println!("{}",a * b);
+}
+//fn main() {
+//    proconio::input! {
+//        a: i32,
+//        b: i32,
+//    }
+//    println!("{}", a + b);
+//}
