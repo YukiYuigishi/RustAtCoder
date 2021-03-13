15c3e388985e421ca16678159cff468787617c8c
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-03-13 13:10:16 +0900

    ABC161 A問 AC

---
+++ abc161/src/bin/a.rs
@@ -0,0 +1,17 @@
+fn main(){
+    proconio::input! {
+        X:i32,
+        Y:i32,
+        Z:i32,
+
+
+
+    }
+
+    println!("{} {} {}",Z,X,Y);
+
+
+
+
+
+}
---
+++ abc163/src/bin/c.rs
@@ -0,0 +1,11 @@
+fn main(){
+    proconio::input! {
+        A:i32,
+        B:i32,
+        H:i32,
+        M:i32,
+    }    
+
+
+    
+}
